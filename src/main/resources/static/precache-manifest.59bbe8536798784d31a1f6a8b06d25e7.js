a2c7c46fccaecd612e26f5bbd84c0d31068180b8
avri1985 <avriyanggupane@gmail.com>
2020-03-07 14:02:36 +0700

    react-client

---
+++ src/main/resources/static/precache-manifest.59bbe8536798784d31a1f6a8b06d25e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b041b8dca4a6b1b30606bec8509b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78cd0552cf5b7ff35c2a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "97df0fd4efc5b70168e1",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "78cd0552cf5b7ff35c2a",
+    "url": "/static/js/2.23b96713.chunk.js"
+  },
+  {
+    "revision": "97df0fd4efc5b70168e1",
+    "url": "/static/js/main.588cb975.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
